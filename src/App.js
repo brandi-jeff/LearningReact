36ca7dd122b32c25905520b0f8e07ba60371480e
brandi.jeff <brandi.jeff2@gmail.com>
2023-03-02 10:15:56 -0500

    add default styles for pages. added web kits for animations. added media queries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Footer, Blog, Features, WhatGPT3, Header, Possibility } from './containers';
 import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
